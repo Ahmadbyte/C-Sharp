b0222ad891f5338135d2a1f67ab1f9b4da34d97a
Ahmadbyte <shariqueahmad011@gmail.com>
2024-10-30 07:37:20 +0530

    More Commit

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -6,10 +6,11 @@ namespace MyConsoleApp
     {
         static void Main(string[] args)
         {
-            Calculatefactorial();
-            EvenOdd();
-            Palindrome();
-            PalString();
+            // Calculatefactorial();
+            // EvenOdd();
+            // Palindrome();
+            // PalString();
+            SumOfArray();
         }
         static void Calculatefactorial()
         {
@@ -82,5 +83,143 @@ namespace MyConsoleApp
                 Console.WriteLine("The string is not a palindrome : " + rstr);
             }
         }
+
+        static void SumOfArray()
+        {
+            Console.WriteLine("Enter the size of array : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum =0;
+            int [] numbers = new int[n];
+            Console.WriteLine("Enter an array of numbers to find sum of digits:");
+
+            for(int i = 0; i < n; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+                sum += numbers[i];
+            }
+            // foreach (var num in numbers) //another method to find sum of digits
+            // {
+            //     sum += Convert.ToInt32(num);
+            // }   
+            Console.WriteLine("Sum of digits: " + sum);
+        }
     }
 }
+
+/*---------------Abstract OOps---------
+
+using System;
+
+public abstract class Animal
+{
+    public abstract void Sound();
+    
+    public void Eat(){
+        Console.WriteLine("Animal Eat");
+    }
+}
+public class Dog : Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Dog Barks");
+    }
+}
+public class Tiger : Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Tiger Roar");
+    }
+}
+public class Program{
+    public static void Main(string[] args){
+        Animal dog = new Dog();
+        dog.Sound();
+        dog.Eat();
+        
+        Animal tiger = new Tiger();
+        tiger.Sound();
+        tiger.Eat();
+    }
+}
+-----------------Inheritance OOps----------------
+
+using System; //Inheritance
+
+public class Teacher //Base class
+{
+    public string Name
+    {
+        get;
+        set;
+    }
+    public string Name2
+    {
+        get;
+        set;
+    }
+    public string StudentName
+    {
+        get;
+        set;
+    }
+    public string StudentName2
+    {
+        get;
+        set;
+    }
+    public void Subject()
+    {
+        Console.WriteLine($"{Name} is teaching Science");
+    }
+    public void Subject2()
+    {
+        Console.WriteLine($"{Name2} is teaching Biology");
+    }
+    public void Timing()
+    {
+        Console.WriteLine($"{Name} is teaching 2 hours");
+    }
+    public void Timing2()
+    {
+        Console.WriteLine($"{Name2} is teaching 3 hours");
+    }
+}
+
+public class Student : Teacher //Derived Class
+{
+    public void Science()
+    {
+        Console.WriteLine($"{StudentName} is Studying Science from {Name}");
+    }
+}
+
+public class Student2 : Teacher //Derived Class
+{
+    public void Biology()
+    {
+        Console.WriteLine($"{StudentName2} is Studying Biology from {Name2}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student obj = new Student();
+        obj.Name = "Sushmita";
+        obj.StudentName="Sharique";
+        obj.Subject();
+        obj.Timing();
+        obj.Science();
+        
+        Student2 obj1 = new Student2();
+        obj1.Name2 = "Rashmi";
+        obj1.StudentName2="Ahmad";
+        obj1.Subject2();
+        obj1.Timing2();
+        obj1.Biology();
+    }
+}
+*/
